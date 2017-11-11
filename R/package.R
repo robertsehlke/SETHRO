a4d8e38eeb163832bf91d0b541c9ada95d46c018
robertsehlke <rsehlke@gmx.de>
2017-11-11 15:10:26 +0100

    minor change to DESCRIPTION

---
+++ R/package.R
@@ -0,0 +1,21 @@
+#' @title Package SETHRO
+#' 
+#' @description 
+#' Simple Enrichment Testing for Highly Redundant Ontologies
+#' 
+#' @details
+#' Functions:\cr
+#' \code{\link{clusteredEnrichmentGO}} - calculate gene set enrichment with subsequent clustering of redundant terms.\cr
+#' \code{\link{createAnnotationMatrix}} - creates a sparse annotation matrix from a names list of gene identifiers.\cr
+#' \code{\link{createGoMatrix}} - creates a sparse annotation matrix using a bioconductor annotation package.\cr
+#' \code{\link{plotClusteredEnrichment}} - plots primary terms from a \link{clusteredEnrichmentGO} result.\cr 
+#' 
+#' @name SETHRO-package
+#' @aliases SETHRO
+#' 
+#' @author 
+#' Robert Sehlke
+#' 
+#' @docType package
+
+NULL
